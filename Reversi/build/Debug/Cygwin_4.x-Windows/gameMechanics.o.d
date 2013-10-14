20d2c45b6dadd8e0675ffe41163eb6893720ba43
Cory Maughmer <cory.maughmer@neo.tamu.edu>
2013-10-14 18:27:58 -0500

    Updated makefile to include socket code

--- Reversi/build/Debug/Cygwin_4.x-Windows/gameMechanics.o.d
+++
@@ -1,6 +0,0 @@
-build/Debug/Cygwin_4.x-Windows/gameMechanics.o: gameMechanics.cpp \
- gameMechanics.h stdhf.h
-
-gameMechanics.h:
-
-stdhf.h:
--- Reversi/build/Debug/Cygwin_4.x-Windows/gameServer.o.d
+++
@@ -1,8 +0,0 @@
-build/Debug/Cygwin_4.x-Windows/gameServer.o: gameServer.cpp gameServer.h \
- stdhf.h gameMechanics.h
-
-gameServer.h:
-
-stdhf.h:
-
-gameMechanics.h:
